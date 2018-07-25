d26e102968219389f1cc3cc629a67563b3ee1f6c
Administrator <kiro.alper@algorithmz.net>
2018-07-25 15:36:46 +0200

    Gmail Login

--- app/src/main/java/com/example/administrator/gmaillogin/MainActivity.kt
+++ app/src/main/java/com/example/administrator/gmaillogin/MainActivity.kt
@@ -2,11 +2,57 @@ package com.example.administrator.gmaillogin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import android.view.View
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.R.attr.data
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import com.google.android.gms.common.api.ApiException
+
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+       var  mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        google_sign.setOnClickListener(View.OnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent,1)
+
+        })
+
+
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+       // super.onActivityResult(requestCode, resultCode, data)
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+        textView.text=task.result.email
+       // handleSignInResult(task)
+    }
+
 }
